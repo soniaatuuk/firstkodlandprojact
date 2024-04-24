4caddd3ebd89dacfa53e6e37f86a46e16dffb64f
soniakodlandgit <167987022+soniakodlandgit@users.noreply.github.com>
2024-04-24 21:24:31 +0800

    Create kamusgaul.py

---
+++ kamusgaul.py
@@ -0,0 +1,16 @@
+meme_dict = {
+            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
+            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
+            "ROFL" : "tanggapan terhadap lelucon",
+            'SHEESH' : 'sedikit ketidaksetujuan',
+            "KEY" : "VALUE"
+            }
+
+word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+
+if word in meme_dict.keys():
+    # Apa yang harus kita lakukan jika kata itu ditemukan?
+    print(meme_dict[word])
+else:
+    # Apa yang harus kita lakukan jika kata itu tidak ditemukan?
+    print("???")
